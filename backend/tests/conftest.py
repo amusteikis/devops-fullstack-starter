1963f97370d4cbfcdb55b02f1ebd4e227f4d5bd1
Agustin Musteikis <amusteikis@hotmail.com>
2025-06-17 18:16:23 +0000

    Fix: create tables before running test

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
-from backend_app import app as flask_app
+from backend_app import app as flask_app, db
 
 @pytest.fixture
 def client():
     flask_app.config["TESTING"] = True
+    with flask_app.app_context():
+        db.create_all()
     with flask_app.test_client() as client:
         yield client
